cf57b3cd30925a506f6858e0a4abd3f23706c9de
Alexander Baker <alex.baker685@gmail.com>
2019-02-18 22:23:14 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+class Artist
+
+  attr_accessor :name, :genre
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    binding.pry
+    @@all << self
+  end
+
+  def new_song(name, genre)
+    @genre = genre
+    song = Song.new(name, self, genre)
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :song, :genre, :artist
+
+  @@all = []
+
+  def initialize(name, artist, genre)
+    @artist = artist
+    @genre = genre
+    @name = name
+    @@all << self
+  end
+
+  def genre
+    genre = Genre.new(genre)
+  end
+
+  def artist
+    artist = Artist.new(artist)
+  end
+
+
+  def self.all
+    @@all
+  end
+
+end
